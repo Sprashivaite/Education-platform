7777442a4dc0bc2070ffdc0e05f77688660c9dbe
Arkadiy Telitsyn <sanitar.kem@mail.ru>
2023-12-14 20:30:03 +0500

    fix deploy

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,6 +1,6 @@
 const SERVER_BASE_URL =
   process.env.NODE_ENV === "production"
-    ? "https://education-platform-4cm3.onrender.com/"
+    ? "https://education-platform-4cm3.onrender.com"
     : "http://localhost:3001";
 
 export default SERVER_BASE_URL;
